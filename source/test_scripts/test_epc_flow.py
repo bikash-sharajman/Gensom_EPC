1410a22eaf65281d8d98c0a16a9ccf278d9769cd
bikash-sharajman <bikashsahoo595@gmail.com>
2026-08-05 14:55:44 +0530

    update in test flow

--- source/test_scripts/test_epc_flow.py
+++ source/test_scripts/test_epc_flow.py
@@ -5,12 +5,9 @@ from source.config.config_reader import cr
 @pytest.mark.testing
 def test_epc_positive_flow(setup):
     setup.epc_login.login(cr.email, cr.password)
-    # setup.project_init.navigate_to_project_initialization()
-    # p_code = setup.project_init.inititate_new_project()
-    p_code = "PRJ-KL-2026-0020"
-    # setup.survey.navigate_to_survey_page()
-    # setup.survey.assign_new_survey(p_code)
-    # setup.survey.submit_survey_report(p_code)
+    setup.project_init.navigate_to_project_initialization()
+    p_code = setup.project_init.inititate_new_project()
+    # p_code = "PRJ-KL-2026-0020"
     # setup.engg_page.navigate_to_engineering_page()
     # setup.engg_page.click_on_start_design_of_project(p_code)
     # setup.engg_page.start_new_engineering_design()
@@ -18,6 +15,9 @@ def test_epc_positive_flow(setup):
     setup.token_page.navigate_to_token_receipt_page()
     setup.token_page.process_new_receipt(p_code)
     setup.token_page.process_receipt_verification(p_code)
+    setup.survey.navigate_to_survey_page()
+    setup.survey.assign_new_survey(p_code)
+    setup.survey.submit_survey_report(p_code)
     
     
     
